7a76b311375daed9d556fb6377f7da1e68b4ec65
swol1 <den.gavrilin1@gmail.com>
2019-02-02 18:41:40 +0300

    tubi to empty

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  Rails.application.routes.default_url_options = {host: 'tubi.ru'}
+  Rails.application.routes.default_url_options = {host: 'emptybox.ru'}
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -65,7 +65,7 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'emptylink.ru' }
+  config.action_mailer.default_url_options = { host: 'emptybox.ru' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
